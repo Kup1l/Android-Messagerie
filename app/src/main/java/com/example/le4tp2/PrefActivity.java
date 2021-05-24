edd8f153ed6dba6fab7a6fab1332d95da86f2d5a
Thomas De Maen <mr-sylar59@world-of-sylar.fr>
2021-05-24 16:23:52 +0200

    Night theme

--- app/src/main/java/com/example/le4tp2/PrefActivity.java
+++ app/src/main/java/com/example/le4tp2/PrefActivity.java
@@ -1,20 +1,41 @@
 package com.example.le4tp2;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.preference.PreferenceActivity;
+
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.annotation.Nullable;
+import androidx.preference.PreferenceManager;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 
 @EActivity(R.layout.activity_pref)
-public class PrefActivity extends FragmentActivity {
+public class PrefActivity extends FragmentActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    SharedPreferences sp;
 
-        //Amélioration avec fragment
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("theme")) {
+            boolean dark = sharedPreferences.getBoolean("theme", false);
+            if (dark)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+            recreate();
+        }
+    }
 
+    @AfterViews
+    public void create() {
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.registerOnSharedPreferenceChangeListener(this);
+    }
 }
